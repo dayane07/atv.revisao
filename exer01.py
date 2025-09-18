c47c1dd225331b7c73ffa0501af8bacfe20b541b
dayane07 <dayaanez.9008@gmail.com>
2025-09-18 15:47:59 -0300

    nota

--- exer01.py
+++ exer01.py
@@ -8,6 +8,18 @@ voto = int(input("Por favor,digite seu voto:"))
 while voto not in [10,20,30,98,99]:
   voto = int(input("Voto inválido. Por favor, digite um voto válido:"))
   
+if voto == candidatoA:
+  print("Voto válido, voto para candidato A")
+elif voto == candidatoB:
+  print("Voto válido, voto para candidato B")
+elif voto == candidatoC:
+  print("Voto válido, voto para candidato C")
+elif voto == nulo:
+  print("Válido, voto nulo")
+elif voto == branco:
+  print("Válido, voto branco")
+
 else:
   print("Voto válido.")
+
 
